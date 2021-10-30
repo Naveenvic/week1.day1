d432b0879a0a650b0aa96691ed16bf782eb5d769
Naveenvic <93435011+Naveenvic@users.noreply.github.com>
2021-10-30 19:54:12 +0530

    Add files via upload

---
+++ ConvertedProject.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class ConvertedProject {
+
+	public static void main(String[] args) {
+		int number=-40;
+		if (number<0) {
+			System.out.println("The number-40 is converted to 40");
+			
+		}
+
+	}
+
+}
